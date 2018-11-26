a046ac69c04abf75100a479c2a893b921eb545d1
P-Gill97 <pgill@csus.edu>
2018-11-26 00:33:32 -0800

    removed try block

--- mavenModule/src/main/java/JGitApi/CloneRepo.java
+++ mavenModule/src/main/java/JGitApi/CloneRepo.java
@@ -1,5 +1,6 @@
 package JGitApi;
-import org.eclipse.jgit.*;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,9 +9,12 @@ public class CloneRepo {
 
 
 
-    public void clone(String uri) throws IOException {
+    public void clone(String uri) throws IOException , GitAPIException {
         File localPath = File.createTempFile("GitRepo","");
-
+         Git cloneResult = Git.cloneRepository()
+            .setURI(uri)
+            .setDirectory(localPath)
+            .call();
     }
 
 }
