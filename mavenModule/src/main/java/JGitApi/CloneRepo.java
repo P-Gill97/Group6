72ce19f34e7676e651d93971728ee1acab6565aa
P-Gill97 <pgill@csus.edu>
2018-11-26 00:24:02 -0800

    adding clone repo class and dependencies

---
+++ mavenModule/src/main/java/JGitApi/CloneRepo.java
@@ -0,0 +1,16 @@
+package JGitApi;
+import org.eclipse.jgit.*;
+import java.io.File;
+import java.io.IOException;
+
+public class CloneRepo {
+
+
+
+
+    public void clone(String uri) throws IOException {
+        File localPath = File.createTempFile("GitRepo","");
+
+    }
+
+}
