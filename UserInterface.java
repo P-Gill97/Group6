20634704a5b61227115789fcdf937953808f6cbc
Brent Paterson <brentpaterson@csus.edu>
2018-11-25 23:05:16 -0800

    UI started, create the frame

---
+++ UserInterface.java
@@ -0,0 +1,23 @@
+public class UserInterface {
+    private JFrame frame;
+
+    // START TEST CODE //////////////////////////////////////
+
+    // END TEST CODE ////////////////////////////////////////
+
+
+
+    void run() {
+        // need to input database
+
+        // call to get metrics?
+    }
+
+    void init() {
+        frame = new JFrame("Group 6 GitHub Metrics"); // name can be changed later. Or this is fine.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true); // move this?
+    }
+
+}
