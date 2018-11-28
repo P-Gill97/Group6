031062bc77376215cc82b919f59589da4b5ba6d7
P-Gill97 <pgill@csus.edu>
2018-11-27 18:01:36 -0800

    " create an instance of the cloner in the git controller class and made some changes to CloneRepo class"

--- mavenModule/src/main/java/JGitApi/CloneRepo.java
+++ mavenModule/src/main/java/JGitApi/CloneRepo.java
@@ -3,18 +3,32 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import java.io.File;
 import java.io.IOException;
-
+/*
+*  Programmer : Perry Gill
+*  purpose : TO clone a repository
+*
+*
+*  */
 public class CloneRepo {
 
 
 
 
-    public void clone(String uri) throws IOException , GitAPIException {
-        File localPath = File.createTempFile("GitRepo","");
-         Git cloneResult = Git.cloneRepository()
-            .setURI(uri)
-            .setDirectory(localPath)
-            .call();
+    public void clone(String uri)    {
+        File localPath = null;
+        try {
+            localPath = File.createTempFile("GitRepo","");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Git cloneResult = Git.cloneRepository()
+                .setURI(uri)
+                .setDirectory(localPath)
+                .call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- mavenModule/src/main/java/JGitApi/gitController.java
+++ mavenModule/src/main/java/JGitApi/gitController.java
@@ -1,4 +1,14 @@
 package JGitApi;
 
+
 public class gitController {
+   // CloneRepo repoClone =
+    public GitRepository enterURL(String url){
+
+        CloneRepo cr = new CloneRepo();
+        cr.clone(url);
+
+        return null; // Need to add the rest of the function
+    }
+
 }
