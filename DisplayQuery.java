250a21d40d493903b0494d4015f6eb427f764266
rafaelgarcia <rafaelgarcia9212@gmail.com>
2018-11-29 23:37:48 -0800

    inserted all imports needed

---
+++ DisplayQuery.java
@@ -0,0 +1,21 @@
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+import java.util.regex.PatternSyntaxException;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTable;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.RowFilter;
+import javax.swing.table.TableRowSorter;
+import javax.swing.table.TableModel;
