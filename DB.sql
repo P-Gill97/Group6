b6f4a1af1bb594559768c3f9d79be8f4eacb55d9
rafaelgarcia <rafaelgarcia9212@gmail.com>
2018-11-29 23:52:19 -0800

    database used to test connection and variable input

---
+++ DB.sql
@@ -0,0 +1,13 @@
+create schema csc_131;
+
+CREATE TABLE Files
+( FileName			varchar(20) 	NOT NULL,
+  totChar			int,
+  totCount      	int,
+  totWord       	int,
+  totComtrack   	int,
+  totSourceTrack 	int,
+  totalOperators    int,
+  totalOperands     int);
+  
+     
