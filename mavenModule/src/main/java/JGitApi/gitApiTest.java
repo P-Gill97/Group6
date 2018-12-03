7537d0ee58ae58a73beb305fda141e2bb3e5e11a
P-Gill97 <pgill@csus.edu>
2018-12-03 00:06:58 -0800

    Called the functions in test.

--- mavenModule/src/main/java/JGitApi/gitApiTest.java
+++ mavenModule/src/main/java/JGitApi/gitApiTest.java
@@ -8,8 +8,17 @@ public class gitApiTest {
         gitController Controller = new gitController();
         try {
             GitRepository GitRepo = new GitRepository(Controller.getRepo(url)); // calling GitRepository constructor.
+
+            GitRepo.getCFiles();
+            GitRepo.getJavaFiles();
+
+            //Todo: add a for loop to display every item of the Cfiles array list and javaFiles array list.
+
+
+
         } catch (GitAPIException e) {
             e.printStackTrace();
         }
+
     }
 }
