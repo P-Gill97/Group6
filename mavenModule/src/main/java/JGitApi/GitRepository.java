fcd38f1c2a0f1455afbe238b02d580ba81b04b32
P-Gill97 <pgill@csus.edu>
2018-12-02 17:49:33 -0800

    Making Repository object that will hold fucntions to read the repository.

--- mavenModule/src/main/java/JGitApi/GitRepository.java
+++ mavenModule/src/main/java/JGitApi/GitRepository.java
@@ -3,6 +3,12 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
 import java.util.ArrayList;
+/*
+contributor: Perry Gill
+Purpose: To Have an object of repository that holds the repo file and containts all the functions
+needed to get the files needed from the repo. This will be called in the controller class
+that the metrics part of the app can call.
+ */
 public class GitRepository {
     private File repo;
     private ArrayList<File> fileList = new ArrayList<>();
--- mavenModule/src/main/java/JGitApi/gitController.java
+++ mavenModule/src/main/java/JGitApi/gitController.java
@@ -3,7 +3,11 @@ package JGitApi;
 import java.io.File;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-
+/*
+contributor: Perry Gill
+Purpose:  This is the controller class
+that the metrics part of the app can call.
+ */
 public class gitController {
     // directory where repos are saved for testing.
      private String fileDirectory = "/Users/pdippygill/desktop/DownloadedGitRepos";
