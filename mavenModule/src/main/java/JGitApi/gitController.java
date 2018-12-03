b538a8a4297aa68f17d4af57ece7e7322e2f6eae
P-Gill97 <pgill@csus.edu>
2018-12-02 16:31:15 -0800

    Changed method signiture to take file directory as a argument for testing.

--- mavenModule/src/main/java/JGitApi/gitController.java
+++ mavenModule/src/main/java/JGitApi/gitController.java
@@ -2,14 +2,16 @@ package JGitApi;
 
 import java.io.File;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class gitController {
-
-   // CloneRepo repoClone =
-    public File  getRepo(String url){
+    // directory where repos are saved for testing.
+     private String fileDirectory = "/Users/pdippygill/desktop/DownloadedGitRepos";
+    // CloneRepo repoClone =
+    public File  getRepo(String url) throws GitAPIException {
         gitFile gitFileMaker= new gitFile();
 
-        gitFileMaker.getRepo(url);
+        gitFileMaker.getRepo(url , fileDirectory);
 
         return null; // Need to add the rest of the function
     }
