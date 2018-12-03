b2bec6d35859b47cec2ebb626ac8ad3a1cff905d
P-Gill97 <pgill@csus.edu>
2018-12-02 23:58:34 -0800

    Making an instance of gitRepository in test class so i can test git integration.

--- mavenModule/src/main/java/JGitApi/gitApiTest.java
+++ mavenModule/src/main/java/JGitApi/gitApiTest.java
@@ -1,4 +1,15 @@
 package JGitApi;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 public class gitApiTest {
+    public static void main(String[] args){
+        String url = "";
+        gitController Controller = new gitController();
+        try {
+            GitRepository GitRepo = new GitRepository(Controller.getRepo(url)); // calling GitRepository constructor.
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+    }
 }
